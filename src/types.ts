e8db98145c4dc6dea7938ac733d228a4550e6eeb
Colby Williams <ColbyLWilliams@gmail.com>
2023-03-23 00:46:42 -0500

    initial stuff

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Environment {
+  catalogItemName: string;
+  catalogName: string;
+  description?: string;
+  environmentType: string;
+  name: string;
+  parameters: object;
+  provisioningState: string;
+  resourceGroupId: string;
+  scheduledTasks: object;
+  tags: object;
+  user: string;
+}
